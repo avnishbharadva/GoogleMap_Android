40b899e2a34dc2fd0a9f952f09ffe679317896c9
Avnish Bharadva <avnishbharadva@gmail.com>
2023-01-13 19:39:11 +0530

    Add Google Map

---
+++ app/src/main/java/com/example/mygooglemaps/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.mygooglemaps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    public static final int PERMISSION_REQUEST_CODE = 9001;
+    private boolean mLocationPermissionGranted;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Check Location Permission
+        if (mLocationPermissionGranted) {
+            Toast.makeText(this, "Ready To Map!", Toast.LENGTH_SHORT).show();
+        } else {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+                }
+        }
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+        supportMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        Toast.makeText(this, "Map Is Ready...", Toast.LENGTH_LONG).show();
+
+    }
+}
